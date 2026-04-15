e6eccbe56e2383e729bc2fc8c4d7ccdf99eced61
byt23 <berkayyasar2004@gmail.com>
2026-04-15 14:57:05 +0300

    Initial Commit

---
+++ Data Heist/Data_HeistApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_HeistApp.swift
+//  Data Heist
+//
+//  Created by BERKAY TURAN on 15.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Data_HeistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
